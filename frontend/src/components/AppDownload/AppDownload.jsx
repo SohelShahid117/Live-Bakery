ea65d041f8627bea7b7ed7c9aefc740673736e92
SohelShahid117 <sohelshahid09@gmail.com>
2025-12-14 10:04:29 +0600

    next start from navbar-responsibve-2.05

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./AppDownload.css";
+
+import { assets } from "./../../assets/assets";
 const AppDownload = () => {
   return (
     <div className="app-download" id="app-download">
       <p>
         For better experience download <br /> Live Bakery App
       </p>
+      <div className="app-download-platforms flex justify-center">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
     </div>
   );
 };
