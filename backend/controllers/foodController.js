c0d85a3e34454f4f0bf78694a9bc05e812f07dc5
SohelShahid117 <sohelshahid09@gmail.com>
2025-12-15 19:10:41 +0600

    insert data to mongoDB-4:10

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,22 @@
+import foodModel from "../models/foodeModel.js";
+import fs from "fs";
+
+//add food item
+const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    image: image_filename,
+    category: req.body.category,
+  });
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added" });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+export { addFood };
