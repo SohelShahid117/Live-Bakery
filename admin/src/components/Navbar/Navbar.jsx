a4f21f7f26c626da668d145ec99d246ff06dd920
SohelShahid117 <sohelshahid09@gmail.com>
2025-12-16 00:40:02 +0600

    start from navbar-4:35

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+
+const Sidebar = () => {
+  return <div></div>;
+};
+
+export default Sidebar;
