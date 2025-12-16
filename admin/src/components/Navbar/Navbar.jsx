2edf9a1f5e7cb595c06765c7b3a1194bd699b165
SohelShahid117 <sohelshahid09@gmail.com>
2025-12-16 13:55:34 +0600

    now start add.css-5:02

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from "react";
-import Navbar from "./Navbar";
+import "./Navbar.css";
+
+import { assets } from "./../../assets/assets";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <p className="logo">
+        <span
+          to="/"
+          className="text-5xl font-bold text-amber-600 w-auto cursor-pointer"
+        >
+          Live Bakery
+        </span>
+        <br />
+        <span>Admin</span>
+      </p>
+      {/* <img className="logo" src={assets.logo} alt="" /> */}
+      <img className="profile" src={assets.profile_image} alt="" />
+    </div>
+  );
 };
 
 export default Navbar;
