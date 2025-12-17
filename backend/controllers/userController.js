b05ae82d74e3a4c3d1cbc2248690596866681cf5
SohelShahid117 <sohelshahid09@gmail.com>
2025-12-17 15:57:47 +0600

    backend done,now start frontrnd login-6:22

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,10 +4,36 @@ import bcrypt from "bcrypt";
 import validator from "validator";
 
 //loginUser
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exists" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: "Invalid credentials/passwords",
+      });
+    }
+
+    const token = createToken(user._id);
+    if (token) {
+      return res.json({ success: true, token });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
 
 const createToken = (id) => {
-  jwt.sign({ id });
+  return jwt.sign({ id }, process.env.JWT_SECRET);
 };
 
 //registerUser
@@ -43,8 +69,11 @@ const registerUser = async (req, res) => {
       password: hashedPassword,
     });
     const user = await newUser.save();
+    const token = createToken(user._id);
+    res.json({ success: true, token });
   } catch (error) {
     console.log(error);
+    res.json({ success: false, message: "Error" });
   }
 };
 
